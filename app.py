40a47ed8373249bb344cda96c0cf3ccd39efade4
nmschorr <nancyschorr@yahoo.com>
2018-08-25 13:51:52 -0700

    Moved app.py up one level for openshift.
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
 from src.linkcheck import linkcheck
+from flask import Flask, request, render_template
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
 
 
-class app(object):
-    from pathlib import Path
-    filenme = Path("./src/runargs.txt")
-    res, res_list = 1, []
-    with open(filenme, 'r+') as file:
-        while res:
-            res = file.readline()
-            if not res.startswith('#'):
-                res_list.append(res.rstrip())
-    if res_list:
-        res_list.pop(-1)  # last one comes in empty
+#@app.route("/")
+# @app.route('/<string:page_name>/')
+# def hello():
+#     answer = linkcheck('schorrmedia.com')
+#     return str(answer)
 
-    for site in res_list:
-        linkcheck(site)
+@app.route('/<string:page_name>/')
+def static_page(page_name):
+    return render_template('%s.html' % page_name)
 
-app()
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=5000)
+
+
+
+
+#class app(object):
+    # from pathlib import Path
+    # filenme = Path("./src/runargs.txt")
+    # res, res_list = 1, []
+    # with open(filenme, 'r+') as file:
+    #     while res:
+    #         res = file.readline()
+    #         if not res.startswith('#'):
+    #             res_list.append(res.rstrip())
+    # if res_list:
+    #     res_list.pop(-1)  # last one comes in empty
+
+    # " for site in res_list:
+    #linkcheck(site)
+
+#app()
