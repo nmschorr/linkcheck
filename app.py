462785667a6e73f9b6f6acb42355b341eb9e59f2
nmschorr <nancyschorr@yahoo.com>
2018-08-29 17:46:02 -0700

    Looking good.
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-from flask import Flask, flash, session, request, render_template
+from flask import Flask,  session, request, render_template, redirect
 from flask_restful import Resource, Api
 from src.linkcheck import linkcheck
 
-app = Flask(__name__)
 #app.secret_key = "super secret key"
 #app.config['SESSION_TYPE'] = 'memcached'
 #app.config['SECRET_KEY'] = 'super secret key'
+app = Flask(__name__)
 
 
-@app.route('/')
-def run():
-    lc = linkcheck()
-    site = 'schorrmedia.com/mytest.html'
-    answer = lc.main_run(site)
-    thelines = str(answer[0])
-    return render_template('index.html', title='Home', thelines=thelines, parent=site)
+@app.route('/signup', methods = ['POST'])
+def signup():
+    email = request.form['email']
+    print("The email address is '" + email + "'")
+    return redirect('/')
+
+#@app.route('/')
+# def run():
+#     lc = linkcheck()
+#     site = 'schorrmedia.com/mytest.html'
+#     lines = lc.main_run(site)
+#     return render_template('index.html', title='Home', lines=lines, parent=site)
+#
+# def handle_data():
+#     projectpath = request.form['projectFilepath']
+
 
 if __name__ == '__main__':
     app.run(debug=True)
