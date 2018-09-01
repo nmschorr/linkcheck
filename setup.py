ef227a8df5945ce332a4d21137a8e933ce61477e
nmschorr <nancyschorr@yahoo.com>
2018-08-31 18:14:51 -0700

    changed port to 8080 for OpenShift deployment.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='linkcheck',
+      version='1.2',
+      description='Check links from any website',
+      author='Nancy Schorr',
+      author_email='nancyschorr@yahoo.com',
+      url='http://schorrmedia.com',
+      install_requires=['Flask=1.0.2','requests_html=0.9.0'],
+     )
