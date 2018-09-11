d1842818ddf2ef55ad7ebf8fbd9b7ca99be83d4b
nmschorr <nancyschorr@yahoo.com>
2018-09-10 23:33:12 -0700

    New prototype working great for threading.
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- appn.py
+++ appn.py
@@ -3,32 +3,51 @@ from flask import Flask, request, render_template
 from linkcheck import linkcheck
 import time
 from concurrent.futures import ThreadPoolExecutor
-#PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
-#GUNICORN_CMD_ARGS="--bind=0.0.0.0"
+from flask import copy_current_request_context
+import gevent.monkey
+
 
 app = Flask(__name__)
-# to do:  spinner
-exec = ThreadPoolExecutor()
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html')  ## has a form
 
-@app.route('/results', methods = ['POST','GET'])
-def results():
-    time.sleep(1)
+
+
+@app.route('/waiting', methods = ['POST','GET'])
+def waiting():
     name = request.form['name']
-    exec.submit(runme('schorrmedia.com/m.html'))
 
-    return render_template('waiting.html', name=name)
+    def w(name='schorrmedia.com/m.html'):
+        print("waiting")
+        lc = linkcheck()
+        namee = 'schorrmedia.com/m.html'
+        answers = lc.main(namee)
+        print (answers)
+
+    def p():
+        print("hello")
+
+    gevent.joinall([
+        gevent.spawn(w),
+        gevent.spawn(p),
+    ])
+    return render_template('waiting.html')  ## has a form
 
 
-def runme(name='schorrmedia.com/m.html'):
-    lc = linkcheck()
-    answers = lc.main(name)   # later name from form
-    render_template('results.html', answers = answers)
+@app.route('/waiting')
+def printwait():
+    answers = 'waiting'
+    return answers
 
 
+
+
+
+#GUNICORN_CMD_ARGS="--bind=0.0.0.0"
+#PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
 HOST='0.0.0.0'
 HOST='127.0.0.1'
 app.run(host=HOST, port=8080)
