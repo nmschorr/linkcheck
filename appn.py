c6b5d63eaf74037a870cb34cf0039a53c95ab31d
nmschorr <nancyschorr@yahoo.com>
2018-09-10 20:18:28 -0700

    New prototype working great for OpenShift.
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- appn.py
+++ appn.py
@@ -1,9 +1,14 @@
 
 from flask import Flask, request, render_template
 from linkcheck import linkcheck
+import time
+from concurrent.futures import ThreadPoolExecutor
+#PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
+#GUNICORN_CMD_ARGS="--bind=0.0.0.0"
 
 app = Flask(__name__)
 # to do:  spinner
+exec = ThreadPoolExecutor()
 
 @app.route('/')
 def index():
@@ -11,16 +16,21 @@ def index():
 
 @app.route('/results', methods = ['POST','GET'])
 def results():
+    time.sleep(1)
     name = request.form['name']
+    exec.submit(runme('schorrmedia.com/m.html'))
+
+    return render_template('waiting.html', name=name)
+
+
+def runme(name='schorrmedia.com/m.html'):
     lc = linkcheck()
-
-    render_template('waiting.html', name=name)
-    answers = lc.main(name)
-    return render_template('results.html', answers = answers)
+    answers = lc.main(name)   # later name from form
+    render_template('results.html', answers = answers)
 
 
-if __name__ == '__main__':    #don't delete!
-  app.run(debug=True)
-
+HOST='0.0.0.0'
+HOST='127.0.0.1'
+app.run(host=HOST, port=8080)
 
 
