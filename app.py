95d5cfb3342ba0a0b8a0d95862ff3948236acddf
nmschorr <nancyschorr@yahoo.com>
2018-09-22 18:23:09 -0700

    Big bug fixes and reorg.
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- app.py
+++ app.py
@@ -1,64 +1,103 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, g
 import linkcheck
 import threading, time, os
 from jinja2 import Environment, PackageLoader, select_autoescape
 from nocache import nocache
-import app_support_code
-import app_support_conf
 import datetime
-from flask import make_response
+from app_support_code import AppSupport
 
 
+class ProdConfig(object):
+    site = "empty"
+    just_name = "empty"
+    just_stat = "empty"
+    os_donefile = "empty"
+    os_donefile = "empty"
+    reg_os_file_path = "empty"
 
-app = Flask(__name__)
-##app.config['SECRET_KEY'] = 'secret!'
+    def set_site(self, site):
+        self.site = site
+    def get_site(self):
+        return self.site
 
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+    def set_just_name(self, just_name):
+        self.just_name = just_name
+    def get_just_name(self):
+        return self.just_name
+
+    def set_just_stat(self, just_stat):
+        self.just_stat = just_stat
+
+    def get_just_stat(self):
+        return self.just_stat
+
+    def set_os_donefile(self, os_donefile):
+        self.os_donefile = os_donefile
+    def get_os_donefile(self):
+        return self.os_donefile
+
+    def set_reg_os_file_path(self, reg_os_file_path):
+        self.reg_os_file_path = reg_os_file_path
+    def get_reg_os_file_path(self):
+        return self.reg_os_file_path
+
+    def set_osdonefile(self, osdonefile):
+        self.osdonefile = osdonefile
+    def get_osdonefile(self):
+        return self.osdonefile
+
+pc = ProdConfig()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    osroot = app.root_path  # os path
+
+    just_name, just_stat, donef_name, file_os_path_all, os_donefile_path = AppSupport.make_filenames(app, osroot )
+
+    pc.set_just_name(just_name)
+    pc.set_just_name(just_stat)
+    pc.set_just_name(donef_name)
+    pc.set_just_name(file_os_path_all)
+    pc.set_just_name(os_donefile_path)
+    return app
+
+app = create_app()
+
 env = Environment(
     loader=PackageLoader('linkcheck', 'templates'),
     autoescape=select_autoescape(['html', 'xml']),
 )
-gc = app_support_code.hcode_cls()
 
+def notreadyyet(ste):
+    from app_support_code import AppSupport
+    newst= AppSupport.not_ready_msg(ste)
+    just_stat = pc.get_just_stat()
 
-def setup():
-    gc.__init__()
-    reg_os_file_path, osdonefile = gc.ret_ospaths()
-    os_root_path = app.root_path        #os path
-    just_name, just_stat= gc.make_filenames(os_root_path)
-    gc.setname(just_name)
-    gc.setstat(just_stat)
-    print("in init - osdonefile: ", osdonefile)
-
-def notreadyyet():
-    gsite = gc.getgsite()
-    just_stat = gc.getstat()
-    newst= gc.not_ready_msg(gsite)
     fj = open(just_stat, "w")
     fj.write(newst)
     fj.close()
 
-def write_no_err_pg():
-    gsite = gc.getgsite()
-    just_stat = gc.getstat()
-    newstt = gc.fin_msg(gsite)
+def write_no_err_pg(ste):
+    from app_support_code import AppSupport
+    just_stat = pc.get_just_stat()
+    newstt = AppSupport.fin_msg(ste)
     fjj = open(just_stat, "w")
     fjj.write(newstt)
     fjj.close()
 
-
-
 def worker1():   # run LinkCheck and print to console
+    from app_support_code import AppSupport
     lc = linkcheck.LinkCheck()
-    reg_os_file_path, osdonefile = gc.ret_ospaths()
+    reg_os_file_path = pc.get_reg_os_file_path()
+    osdonefile = pc.get_os_donefile()
     print("osdonefile in worker1: ", osdonefile)
-    gsite = gc.getgsite()
-
-    print("inside worker1 thread. you entered: ", gsite)
-    answers = lc.main(gsite)
+    site = pc.get_site()
+    print("inside worker1 thread. you entered: ", site)
+    answers = lc.main(site)
     time.sleep(2)
     if len(answers) > 0:
-        gc.writeres(answers, reg_os_file_path, osdonefile)
+        AppSupport.writeres(answers, reg_os_file_path, osdonefile)
     else:
         print("no errors found")
         write_no_err_pg()
@@ -66,17 +105,10 @@ def worker1():   # run LinkCheck and print to console
     print(dt + "  worker1 done")
 
     #-----------------------------------------------------------------------------
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "-1"
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
+
+
+# def add_header(response):
+#     return response
     #-----------------------------------------------------------------------------
 
 @app.route('/')
@@ -86,20 +118,19 @@ def index():
 @app.route('/results', methods = ['POST','GET'])
 @nocache             # very important so client server doesn'w_thread cache results
 def results():
-    setup()
-    threads = []
-    gsite = request.form['name']
-    gc.setgsite(gsite)
-    just_name = gc.getname()
-    notreadyyet()
-    w_thread = threading.Thread(target=worker1)
-    threads.append(w_thread)
-    w_thread.start()
-    print("just started thread. root path: " + app.root_path + " you entered: ", gsite)
-    # response = make_response(render_template('results.html', name = just_name) )
-    # add_header(response)
-    # return response
-    return render_template('results.html', name = just_name)  ## has a form
+    with app.app_context():
+        site = request.form['name']
+        pc.set_site(site)
+        notreadyyet(site)
+
+        threads = []
+        w_thread = threading.Thread(target=worker1)
+        threads.append(w_thread)
+        w_thread.start()
+        print("just started thread. root path: " + app.root_path + " you entered: ", site)
+
+        just_name = pc.get_just_name()
+        return render_template('results.html', name = just_name)  ## has a form
 
 
 
@@ -112,5 +143,7 @@ HOSTPORT = os.getenv('HOSTPORT', default=8080)
 HOSTPORT = 8080
 print("hostip: " + HOSTIP + "  HOSTPORT: ", HOSTPORT)
 debugnow = os.getenv('debug', default=False)
+
+
 app.run(host=HOSTIP, port=HOSTPORT, debug=True)
 
--- app_support_code.py
+++ app_support_code.py
@@ -1,40 +1,26 @@
 from os import path
 from datetime import datetime
 import app_support_conf
+from flask import g
 
 
+class AppSupport:
 
-class hcode_cls(object):
-    one_para_returns = '<p></p>'
-    two_para_returns = one_para_returns + one_para_returns
-    blank4spaces = "&nbsp;&nbsp;&nbsp;&nbsp;"
-    end_body_html_tags = "</body></html>"
-    ospath_full = 'notset'
-    os_donefile_path = 'notset'
-    just_name_cls = "notset"
-    just_stat_cls = "notset"
-    gsite_cls = "notset"
-
-    def __init__(self):
-        self.os_donefile = "empty"
-        self.ospath_full = 'notset'
-        self.os_donefile_path = 'notset'
-        self.just_name_cls = "notset"
-        self.just_stat_cls = "notset"
-        self.gsite_cls = "notset"
-
-    def fin_msg(self, asite):
+    @staticmethod
+    def fin_msg(asite):
         thishost = app_support_conf.thishost
         print("inside hc. ")
         outst1 = "<!DOCTYPE html><html><head><title>No Broken Links</title></head><body>"
         answer2 =  "No broken links found for " + asite +". Thanks for using LinkCheck."
         homeURL4 = "<a href=" + thishost + ">Start Over</a>"
-        newstt = outst1 + answer2 + self.two_para_returns + homeURL4 + self.end_body_html_tags
+        newstt = outst1 + answer2 + '<p></p>' + homeURL4 + "</body></html>"
         return newstt
 
-    def datalines(self, f,data):
+    @classmethod
+    def datalines(cls, f,data):
         thishost = app_support_conf.thishost
-        home_url = self.blank4spaces + self.blank4spaces + "<h2><a href=" + thishost + ">Start Over</a></h2><p></p>"
+        home_url =  "&nbsp;&nbsp;&nbsp;&nbsp;" +  "&nbsp;&nbsp;&nbsp;&nbsp;" + \
+                "<h2><a href=" + thishost + ">Start Over</a></h2><p></p>"
 
         sm = "http://schorrmedia.com"
         smedia_url = "<h3><a href=" + sm + ">Visit SchorrMedia.com</a>"
@@ -55,26 +41,27 @@ class hcode_cls(object):
             f.write("**ERROR-->  ")
             f.write(str(line[1]))  ## the error
             f.write(spaces)
-            f.write("<br>" + self.blank4spaces + "  ***FOUND ON: ")
+            f.write("<br>" + "&nbsp;&nbsp;&nbsp;&nbsp;" + "  ***FOUND ON: ")
             f.write("<a href='")
             f.write(str(line[2]))
             f.write("'>")
             f.write(str(line[2]))
             f.write("</a>")
-            f.write(self.one_para_returns)
+            f.write('<p></p>')
 
-        f.write(self.two_para_returns)
+        f.write('<p></p>')
         f.write(home_url)
-        f.write(self.blank4spaces)
+        f.write("&nbsp;&nbsp;&nbsp;&nbsp;")
         f.write(smedia_url)
-        f.write(self.blank4spaces)
+        f.write("&nbsp;&nbsp;&nbsp;&nbsp;")
         f.write(git_url)
-        f.write("</div>" + self.end_body_html_tags)
+        f.write("</div>" + "</body></html>")
 
 
-    def not_ready_msg(self, gsite):
-        usr_msg3 = "Results not ready yet." + self.one_para_returns + "You entered: " + gsite + self.one_para_returns + \
-                 "Page will automatically reload until results appear." + self.one_para_returns
+    @classmethod
+    def not_ready_msg(cls, gsite):
+        usr_msg3 = "Results not ready yet." + '<p></p>' + "You entered: " + gsite + '<p></p>' + \
+                 "Page will automatically reload until results appear." + '<p></p>'
 
         arf1 = "<a href="
         jst2 = "javascript:location.reload(true)"
@@ -93,37 +80,31 @@ class hcode_cls(object):
         st1 = "<style>body {padding-left:10em;}</style></head>"
         st2 = "<body>   "
         st2b = "<script>setInterval(checkDoneFile, 2000);</script>"
-
-
-        newst = headr0 + st1 + st2 + st2b + usr_msg3 + refresh4 + self.end_body_html_tags
+        newst = headr0 + st1 + st2 + st2b + usr_msg3 + refresh4 + "</body></html>"
         return newst
 
-    def make_filenames(self, ospath):
+    @classmethod
+    def make_filenames(cls, app, osroot):
         stat = "static"
         timestp = format(datetime.now(), '%Y%m%d%H%M%S')
-        nme_only = "res" + timestp + ".html"
-        nme_plus_static = path.join(stat, nme_only)
         fname_only = "res" + timestp + ".html"
-        self.os_donefile =  "res" + timestp + ".htmldone"
+        with app.app_context():
+            just_name = "res" + timestp + ".html"
+            just_stat = path.join(stat, just_name)
+            donef_name =  "res" + timestp + ".htmldone"
 
-        os_path_plus_stat = path.join(ospath, stat)
-        self.ospath_full = path.join(os_path_plus_stat, fname_only)
-        self.os_donefile_path = path.join(os_path_plus_stat, self.os_donefile)
-        print("make_filenames os_donefile_path: ", self.os_donefile_path)
-
-        return nme_only, nme_plus_static
-
-        #  reg_os_file_path, osdonefile, just_name, just_stat
-
-    def ret_ospaths(self):
-        return self.ospath_full, self.os_donefile_path
+            os_path_plus_stat = path.join(osroot, stat)
+            file_os_path_all = path.join(os_path_plus_stat, fname_only)
+            os_donefile_path = path.join(os_path_plus_stat, donef_name)
+            print("make_filenames os_donefile_path: ", os_donefile_path)
+            return just_name, just_stat, donef_name, file_os_path_all, os_donefile_path
 
 
-
-    def writeres(self, data, fnfull, osdonefile_loc):
+    @classmethod
+    def writeres(cls, data, fnfull, osdonefile_loc):
         f = open(fnfull, "w")
-        f.write(self.one_para_returns)
-        self.datalines(f,data)
+        f.write('<p></p>')
+        cls.datalines(f,data)
         f.close() # file is not immediately deleted because we
         print("reg_os_file_path named: ", fnfull , "f.name: ", f.name)
         print("osdonef in writeres: ", osdonefile_loc)
@@ -131,21 +112,3 @@ class hcode_cls(object):
         fd.write("done")
         fd.close() # file is not immediately deleted because we
 
-
-    def setname(self, jnme):
-        self.just_name_cls = jnme
-
-    def getname(self):
-        return self.just_name_cls
-
-    def setstat(self, nme):
-        self.just_stat_cls = nme
-
-    def getstat(self):
-        return self.just_stat_cls
-
-    def setgsite(self, gse):
-        self.gsite_cls = gse
-
-    def getgsite(self):
-        return self.gsite_cls
