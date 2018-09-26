de14d4fe9a2d5c731d8e7c1979da3e445097d8ca
nmschorr <nancyschorr@yahoo.com>
2018-09-25 21:26:42 -0700

    Many fixes again third time.
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- app.py
+++ app.py
@@ -22,12 +22,14 @@ env = Environment(
 
 def notreadyyet(ste, just_stat):
     newst= ac.not_ready_msg(ste)
+    print("just_stat: ", just_stat)
     fj = open(just_stat, "w")
     fj.write(newst)
     fj.close()
 
 def write_no_err_pg(ste):
     just_stat = pcf.get_just_stat()
+    print("just_stat: ", just_stat)
     newstt = ac.fin_msg(ste)
     fjj = open(just_stat, "w")
     fjj.write(newstt)
@@ -90,6 +92,7 @@ def results():
     w_thread = Thread(target=worker1, args=(site,timestp1, justname))
     threads.append(w_thread)
     w_thread.start()
+    sleep(3)
     #ac.myprint("just started thread. You entered: " + site)
     #except Exception as e:
         #ac.myprint(str(e))
@@ -97,11 +100,12 @@ def results():
 
 # from socket import gethostname
 # thehost = gethostname()
-_DEBUG = getenv('_DEBUG')
+#_DEBUG = getenv('_DEBUG')
 _DEBUG = 0
 #HOSTIP = getenv('HOSTIP')
-
-HOSTIP = '0.0.0.0'
-app.run(host=HOSTIP, port=8080)
+#'127.0.0.1'
+#HOSTIP = '0.0.0.0'
+#app.run(host=HOSTIP, port=8080)
+app.run(host='0.0.0.0', port=8089)
 
 
