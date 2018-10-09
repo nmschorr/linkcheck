bcdf7f2e0ce423922b20e1db1192d5ca4bc7483d
nmschorr <nancyschorr@yahoo.com>
2018-10-09 00:03:40 -0700

    Still fixing caching problem.
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- tests/runner.py
+++ tests/runner.py
@@ -2,18 +2,22 @@ import unittest
 from tests import test_one
 from tests import test_two
 
-## unittest.TextTestRunner(verbosity=2).run(suite)
 
 
 class suitetests(unittest.TestCase):
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
 
-    suite.addTest(unittest.makeSuite(test_one.TestCaseLC))
-    runner = unittest.TextTestRunner(verbosity=2)
-    print(runner.run(suite))
+    suite.addTest(suite.addTests(loader.loadTestsFromModule(test_one)))
+    suite.addTest(suite.addTests(loader.loadTestsFromModule(test_two)))
+    #runner = unittest.TextTestRunner(verbosity=2)
+    #print(runner.run(suite))
+    unittest.TextTestRunner(verbosity=2).run(suite)
 
 suitetests()
+## unittest.TextTestRunner(verbosity=2).run(suite)
 
 if __name__ == '__main__':
      unittest.main()
+
+     #verbosity=2
