6ee2ed374381acc84a4ff501e3460663c3f14e6d
nmschorr <nancyschorr@yahoo.com>
2018-10-11 21:50:39 -0700

    Another big cleanup of duplicates.
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ env = Environment(    # jinja2
 
 #-----------------------------------------------------------------------------
 def main_work():   # run LinkCheck and ac.myprint to console
-    site = pcf.get_site()
+    site = pcf.get_rsite()
     ac.myprint("running main_work")
-    just_name = pcf.get_just_name()
+    just_name = pcf.get_rjustname()
     os_path_plus_stat = path.join(app.root_path, "static")
     file_path = path.join(os_path_plus_stat, just_name)
 
@@ -51,14 +51,27 @@ def index():
 @nocache             # very important so client server doesn'w_thread cache results
 @app.route('/indexn', methods = ['POST', 'GET', 'HEAD'])
 def indexn():  # git name of url, construct names and pages, present page with button to next step
+    import random
+    rand = random.random()
+    rnm = str(rand)[2:0]
+    rjustname = "rjustname" + rnm
+    rtimestp = "rtimestp" + rnm
+    rsite = "rsite" + rnm
+
+    pcf.prod_dict.update({rjustname: "empty"})
+    pcf.prod_dict.update({rtimestp: "empty"})
+    pcf.prod_dict.update({rsite: "empty"})
+
     site = request.form['tsite']  # from index.html
     timestp1 = format(datetime.now(), '%Y%m%d%H%M%S')
     just_name = "res" + timestp1 + ".html"
-    pcf.set_site(site)
-    pcf.set_just_name(just_name)
+
+    pcf.set_rtimestp(timestp1)
+    pcf.set_rsite(site)
+    pcf.set_rjustname(just_name)
     print("----------------------------------------------------")
     print("Starting over. Is it True that setting filename is done? ", just_name)
-    sleep(2)
+    sleep(3)
     return render_template('indexn.html', name = site)  ## has a form
 
 @nocache             # very important so client server doesn'w_thread cache results
@@ -66,15 +79,16 @@ def indexn():  # git name of url, construct names and pages, present page with b
 def indexnn():  # git name of ur
     fname='none'
     # e_unused = request.form['name']  # from indexn.html
-    just_name = pcf.get_just_name()
-    fname = "./static/" + just_name
-    print("fname: ", fname)
+    print("in indexnn getting fname from pcf: ")
+    just_name = pcf.get_rjustname()
+    just_name_st = "./static/" + just_name
+    print("just got just_name_st: ", just_name_st)
     main_work()
     sleep(3)
-    return render_template('indexnn.html', filename=fname)  ## has a form
+    return render_template('indexnn.html', filename=just_name_st)  ## has a form
 
 if __name__ == '__main__':
-    #serve(app)
-    app.run('127.0.0.1', 5000, debug=True)
+    serve(app)
+    #app.run('127.0.0.1', 5000, debug=True)
 
 #    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
--- prodconf.py
+++ prodconf.py
@@ -6,40 +6,30 @@ class ProfConf(object):
         self.prod_dict = dict()
         rand = random.random()
         rnm = str(rand)[2:0]
-        self.rst = "rst" + rnm
-        self.rjn =  "rjn" + rnm
-        self.rfp =  "rfp" + rnm
-        self.rts =  "rts" + rnm
+        self.rjustname =  "rjustname" + rnm
+        self.rtimestp =  "rtimestp" + rnm
+        self.rsite =  "rsite" + rnm
 
-        self.prod_dict.update({self.rst:"empty"})
-        self.prod_dict.update({self.rjn:"empty"})
-        self.prod_dict.update({self.rts:"empty"})
-        self.prod_dict.update({self.rfp:"empty"})
+        self.prod_dict.update({self.rjustname:"empty"})
+        self.prod_dict.update({self.rtimestp:"empty"})
+        self.prod_dict.update({self.rsite:"empty"})
 
-    def set_site(self, site_a):
-        self.prod_dict.update({self.rst: site_a})
-        
-    def get_site(self,):
-        return self.prod_dict.get(self.rst)
+    def set_rsite(self, sita):
+        self.prod_dict.update({self.rsite: sita})
+    def get_rsite(self):
+        return self.prod_dict.get(self.rsite)
     
-    def set_just_name(self,just_name_a):
-        self.prod_dict.update({self.rjn: just_name_a})
+    def set_rjustname(self, jname):
+        self.prod_dict.update({self.rjustname: jname})
+    def get_rjustname(self):
+        return self.prod_dict.get(self.rjustname)
 
-    def get_just_name(self):
-        return self.prod_dict.get(self.rjn)
-
-    def set_file_path(self,fpath):
-        self.prod_dict.update({self.rfp: fpath})
-    def get_file_path(self):
-        return self.prod_dict.get(self.rfp)
-    
-    def set_timestp(self,time_st):
-        self.prod_dict.update({self.rts: time_st})
-    def get_timestp(self):
-        return self.prod_dict.get(self.rts)
+    def set_rtimestp(self,time_st):
+        self.prod_dict.update({self.rtimestp: time_st})
+    def get_rtimestp(self):
+        return self.prod_dict.get(self.rtimestp)
     
     def prod_reset(self):
-        self.prod_dict.update({self.rfp: 'reset'})
-        self.prod_dict.update({self.rts: 'reset'})
-        self.prod_dict.update({self.rst: 'reset'})
-        self.prod_dict.update({self.rjn: 'reset'})
+        self.prod_dict.update({self.rtimestp: 'reset'})
+        self.prod_dict.update({self.rjustname: 'reset'})
+        self.prod_dict.update({self.rsite: 'reset'})
