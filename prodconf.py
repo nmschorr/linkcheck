e1a8488fe59d23e7f7f77becb35ad2f5c7c304ec
nmschorr <nancyschorr@yahoo.com>
2018-10-11 22:21:58 -0700

    Another big cleanup of duplicates.
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- prodconf.py
+++ prodconf.py
@@ -6,23 +6,18 @@ class ProfConf(object):
         self.prod_dict = dict()
         rand = random.random()
         rnm = str(rand)[2:0]
-        self.rjustname =  "rjustname" + rnm
         self.rtimestp =  "rtimestp" + rnm
         self.rsite =  "rsite" + rnm
 
-        self.prod_dict.update({self.rjustname:"empty"})
         self.prod_dict.update({self.rtimestp:"empty"})
         self.prod_dict.update({self.rsite:"empty"})
+        # self.rjustname =  "rjustname" + rnm
+        # self.prod_dict.update({self.rjustname:"empty"})
 
     def set_rsite(self, sita):
         self.prod_dict.update({self.rsite: sita})
     def get_rsite(self):
         return self.prod_dict.get(self.rsite)
-    
-    def set_rjustname(self, jname):
-        self.prod_dict.update({self.rjustname: jname})
-    def get_rjustname(self):
-        return self.prod_dict.get(self.rjustname)
 
     def set_rtimestp(self,time_st):
         self.prod_dict.update({self.rtimestp: time_st})
@@ -31,5 +26,11 @@ class ProfConf(object):
     
     def prod_reset(self):
         self.prod_dict.update({self.rtimestp: 'reset'})
-        self.prod_dict.update({self.rjustname: 'reset'})
         self.prod_dict.update({self.rsite: 'reset'})
+        # self.prod_dict.update({self.rjustname: 'reset'})
+
+
+   # def set_rjustname(self, jname):
+    #     self.prod_dict.update({self.rjustname: jname})
+    # def get_rjustname(self):
+    #     return self.prod_dict.get(self.rjustname)
